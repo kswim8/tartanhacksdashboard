cbfe7cb968f3e5126ed54b237000e521bb741b11
kswim8 <kenhuang100@gmail.com>
2021-01-10 12:24:31 -0500

    Created cards and test events

---
+++ scheduling_cards/lib/eventinfo.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+//import 'package:dart_mongo/dart_mongo.dart' as dart_mongo;
+
+class EventInfo {
+  static final getData = [
+    {
+      'name': 'Hacking Begins!',
+      'description': 'start hacking!',
+      'time': '9:00 AM',
+      'day': 'March 5',
+    },
+    {
+      'name': 'Flutter Workshop!',
+      'description': 'learn flutter!',
+      'time': '2:00 PM',
+      'day': 'March 6',
+    },
+    {
+      'name': 'Dart Workshop!',
+      'description': 'learn dart!',
+      'time': '4:00 PM',
+      'day': 'March 6',
+    },
+    {
+      'name': 'Hacking Ends!',
+      'description': 'submit your projects!',
+      'time': '9:00 PM',
+      'day': 'March 7'
+    },
+  ];
+}
--- scheduling_cards/lib/main.dart
+++ scheduling_cards/lib/main.dart
@@ -1,54 +1,181 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'eventinfo.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Events and Scheduling'),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+                primarySwatch: Colors.blue,
+            ),
+            home: MyHomePage(title: 'Events and Scheduling'),
+        );
+    }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+    MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
+    final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+    bool _isFavorited = true;
+    var eventData = EventInfo.getData;
 
-  @override
-  Widget build(BuildContext context) {
-    // find a way to pull the event information from the database
-    // add them to the lists below: event name, event category, event time, event description
-    final List<String> eventName = <String>['TartanHacks Begins!', 'Flutter Workshop', 'TartanHacks Ends!'];
-    final List<int> category = <int>[500, 200, 500];
-    
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: eventName.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 250,
-              color: Colors.amber[category[index]],
-              child: Center(child: Text('${eventName[index]}')),
-            );
-          }
-      )
-    );
-  }
+    void _toggleFavorite() {
+        setState(() {
+            if (_isFavorited) {
+                _isFavorited = false;
+            } else {
+                _isFavorited = true;
+            }
+        });
+    }
+
+    Widget eventIcon(data) {
+        return Padding(
+            padding: const EdgeInsets.only(left: 1.0),
+            child: Align(
+                alignment: Alignment.centerLeft,
+                child: Icon(
+                    Icons.star_border,
+                    color: Colors.red,
+                    size: 40,
+                )
+            )
+        );
+    }
+
+    Widget eventName(data) {
+        return Align(
+            alignment: Alignment.centerLeft,
+            child: RichText(
+                text: TextSpan(
+                    text: '${data['name']}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 20,
+                    ),
+                    children: <TextSpan> [
+                        TextSpan(
+                            text: '\n${data['description']}',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold
+                        ))
+                    ]
+                )
+            )
+        );
+    }
+
+    Widget eventTime(data) {
+        return Align(
+            alignment: Alignment.topRight,
+            child: RichText(
+                text: TextSpan(
+                    text: '${data['time']}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                        fontSize: 20,
+                    ),
+                    children: <TextSpan> [
+                        TextSpan(
+                            text: '\n${data['day']}',
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                            )
+                        )
+                    ]
+                )
+            )
+        );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        // find a way to pull the event information from the database
+        // add them to the lists below: event name, event category, event time, event description
+
+        return MaterialApp(
+            home: Scaffold(
+                appBar: AppBar(
+                    title: Text(widget.title),
+                ),
+                body: Container(
+                    child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget> [
+                            Expanded(
+                                child: ListView.builder(
+                                    itemCount: eventData.length,
+                                    itemBuilder: (context, index) {
+                                        return Container(
+                                            padding: EdgeInsets.fromLTRB(10,10,10,0),
+                                            height: 220,
+                                            width: double.maxFinite,
+                                            child: Card(
+                                                elevation: 5,
+                                                child: Padding(
+                                                    padding: EdgeInsets.all(7),
+                                                    child: Stack(
+                                                        children: <Widget> [
+                                                            Align(
+                                                                alignment: Alignment.centerRight,
+                                                                child: Stack(
+                                                                    children: <Widget> [
+                                                                        Padding(
+                                                                            padding: const EdgeInsets.only(left:10, top: 5),
+                                                                            child: Column(
+                                                                                children: <Widget> [
+                                                                                    Row(
+                                                                                        children: <Widget> [
+                                                                                            eventIcon(eventData[index]),
+                                                                                            SizedBox(
+                                                                                                height: 10,
+                                                                                            ),
+                                                                                            eventName(eventData[index]),
+                                                                                            SizedBox(
+                                                                                                width: 15,
+                                                                                            ),
+                                                                                            eventTime(eventData[index]),
+                                                                                            SizedBox(
+                                                                                                height: 10,
+                                                                                            ),
+                                                                                        ]
+                                                                                    )
+                                                                                ]
+                                                                            )
+                                                                        )
+                                                                    ]
+                                                                )
+                                                            )
+                                                        ]
+                                                    )
+                                                ),
+                                            )
+                                        );
+                                    }
+                                )
+                            )
+                        ]
+                    )
+                ),
+            ),
+        );
+    }
 }
